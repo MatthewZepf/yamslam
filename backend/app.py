4be4ae568889c4b2ebbebff969e6d9daeb41d7e4
Matt <mzepf@umich.edu>
2024-06-21 00:03:54 -0400

    Setup intial components and dice now do colors correctly

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/roll-dice', methods=['GET'])
+def roll_dice():
+    import random
+    dice = [random.randint(1, 6) for _ in range(5)]
+    return jsonify(dice=dice)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
